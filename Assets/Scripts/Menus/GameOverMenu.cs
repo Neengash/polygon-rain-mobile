4180f4e9a64408666dd3897cf55d8f438ebdc6c3
Neengash <pepmanas93@gmail.com>
2021-02-27 18:20:34 +0100

    GameOver scene - Mobile Ready - Added transition lock

--- Assets/Scripts/Menus/GameOverMenu.cs
+++ Assets/Scripts/Menus/GameOverMenu.cs
@@ -4,58 +4,27 @@ using UnityEngine;
 
 public class GameOverMenu : MonoBehaviour
 {
-    const int
-        PLAY_AGAIN = 0,
-        MAIN_MENU = 1;
-
-    [SerializeField] RectTransform[] options;
-    [SerializeField] RectTransform selector;
     [SerializeField] AudioClip okSound;
-    [SerializeField] AudioClip changeSelectionSound;
-
     AudioSource audioSource;
-
-    int idx;
+    bool locked = false;
 
     void Start() {
         audioSource = GetComponent<AudioSource>();
-        idx = PLAY_AGAIN;
-        updateSelector();
     }
 
-    void Update() {
-        if (Input.GetKeyDown(KeyCode.J)) {
+    public void PlayAgainOption() {
+        if (!locked) {
+            locked = true;
             audioSource.PlayOneShot(okSound);
-            selectOption();
-        } else if (Input.GetKeyDown(KeyCode.W)) {
-            audioSource.PlayOneShot(changeSelectionSound);
-            idx--;
-            updateSelector();
-        } else if (Input.GetKeyDown(KeyCode.S)) {
-            audioSource.PlayOneShot(changeSelectionSound);
-            idx++;
-            updateSelector();
+            SceneTransitioner.singleton.gotoScene(Scenes.MAIN_GAME);
         }
     }
 
-    protected void updateSelector() {
-        if (idx > options.Length-1) { idx = 0; }
-        if (idx < 0) { idx = options.Length - 1; }
-
-        selector.anchoredPosition = new Vector2(
-            selector.anchoredPosition.x,
-            options[idx].anchoredPosition.y
-            );
-    }
-
-    protected void selectOption() {
-        switch (idx) {
-            case PLAY_AGAIN:
-                SceneTransitioner.singleton.gotoScene(Scenes.MAIN_GAME);
-                break;
-            case MAIN_MENU:
-                SceneTransitioner.singleton.gotoScene(Scenes.MAIN_MENU);
-                break;
+    public void MainMenuOption() {
+        if (!locked) {
+            locked = true;
+            audioSource.PlayOneShot(okSound);
+            SceneTransitioner.singleton.gotoScene(Scenes.MAIN_MENU);
         }
     }
 }
